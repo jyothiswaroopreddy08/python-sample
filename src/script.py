dd37a3a467fe4ec128d6b7f06d2c60b4430371cf
Jyothi Swaroop Reddy <83419758+jyothiswaroopreddy08@users.noreply.github.com>
2023-06-15 10:01:20 +0530

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def helloWorld():
+    return 'Hello World'
+
+@app.route('/demo')
+def demo():
+    return 'Welcome to python demo!'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
