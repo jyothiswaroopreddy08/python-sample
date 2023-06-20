51b87c748c047a66aef1a8329ebccd193368e94f
Jyothi Swaroop Reddy <83419758+jyothiswaroopreddy08@users.noreply.github.com>
2023-06-20 16:12:02 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,17 +1,5 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def helloWorld():
-#     return 'Hello World'
-
-# @app.route('/demo')
-# def demo():
-#     return 'Welcome to python demo!'
-
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=5000)
-
 def add_numbers(a, b):
     return a + b
+
+def multiply_numbers(a, b):
+    return a * b
